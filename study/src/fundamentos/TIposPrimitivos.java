30f5bdf5792b630f3e4ff8177db73678b310dfd0
di0x1 <ricardogamer194@gmail.com>
2026-02-11 14:34:40 -0300

    primitive

--- study/src/fundamentos/TIposPrimitivos.java
+++ study/src/fundamentos/TIposPrimitivos.java
@@ -3,7 +3,27 @@ package fundamentos;
 public class TIposPrimitivos {
     public static void main(String[] args){
 
-        byte anosdeempresa = 128;
+        byte bb = 23;
+        short ss = 542;
+        int ii = 56789;
+        long ll = 3_134_845_223L;
+        //===============================================
+
+        float ff = 11_445.44F;
+        double dd = 2_991_707_103.01;
+
+        //===============================================
+
+        boolean bo = false; //true
+
+        //===============================================
+
+        char pal = 'a';
+
+
+
+
+
 
     }
 }
