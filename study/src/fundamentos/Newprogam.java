c6ee907d4ab06fbcb447fa447c82bbadc58d40bf
Ricardo Iuji <138956575+di0x1@users.noreply.github.com>
2026-02-11 14:15:02 -0300

    Add files via upload

---
+++ study/src/fundamentos/Newprogam.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class Newprogam {
+
+    public static void main(String[] args){
+
+        System.out.printf("Hello word studys in travel!!");
+
+
+    }
+}
---
+++ study/src/fundamentos/temperatura.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+public class temperatura {
+    public static void main(String[] args){
+
+int i;
+
+    for(i=0; i<3; i++){
+
+            double cel = 20;
+            double far = 0;
+            final double div = 9.0 / 5.0;
+            final int soma = 32;
+
+            far = (cel * div) + soma;
+
+            System.out.printf("Temperatura em far %.2f \n", far);
+            pika();
+        }
+    }
+    public static void pika(){
+
+        System.out.printf("olu\n");
+
+    }
+}
