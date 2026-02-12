1fed21e8c9b64596de8cc1e7078de550a022d015
di0x1 <ricardogamer194@gmail.com>
2026-02-11 21:07:03 -0300

    corrigido

--- study/src/fundamentos/temperatura.java
+++ study/src/fundamentos/temperatura.java
@@ -1,26 +1,28 @@
 package fundamentos;
 
 public class temperatura {
-    public static void main(String[] args){
 
-int i;
+    public static void main(String[] args) {
 
-    for(i=0; i<3; i++){
+        int i;
+
+        for (i = 0; i < 3; i++) {
 
             double cel = 20;
             double far = 0;
-            final double div = 9.0 / 5.0;
-            final int soma = 32;
 
-            far = (cel * div) + soma;
-            pika();
+            far = pika(cel);
 
             System.out.printf("Temperatura em far %.2f \n", far);
         }
+
     }
-    public static void pika(){
 
-        System.out.printf("0 \n");
+    public static double pika(double cel) {
 
+        double div = 9.0 / 5.0;
+        int soma = 32;
+
+        return (cel * div) + soma;
     }
 }
