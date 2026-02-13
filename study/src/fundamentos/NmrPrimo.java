07c2d97747bb6532ff9e50bef4ad28fa5681a28e
di0x1 <ricardogamer194@gmail.com>
2026-02-13 01:25:37 -0300

    13/02

--- study/src/fundamentos/NmrPrimo.java
+++ study/src/fundamentos/NmrPrimo.java
@@ -4,17 +4,32 @@ import java.util.Scanner;
 
 public class NmrPrimo {
 
-    public static ehPrimo(int nmr){
+    public static void main(String[] args) {
 
-
-    }
-    public static void main(String[] args){
-
-        int nmr;
-        Scanner = teclado new Scanner(System.in);
-
+        int i;
+        boolean rec;
+        Scanner teclado = new Scanner(System.in);
 
 
 
     }
+
+
+
+    public static boolean ehPrimo(int numero){
+        int i,x=0;
+        for(i=1;i<=numero;i++){
+            if(numero%i==0){
+               x++;
+            }
+        }
+        if(x == 2){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+
 }
+
